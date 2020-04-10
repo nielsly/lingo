a37dc2dfcdd75d25bfc2dff874d79fe2f87e2b0c
Niels Gorter <nierls@live.nl>
2020-04-10 20:01:15 +0200

    Actually fix pathing

--- parsing/php/parse.php
+++ parsing/php/parse.php
@@ -1,7 +1,7 @@
 <?php 
     $data = $_POST['jsonString'];
     $name = $_POST['fileName'];
-    $file = "/words/" + $fileName + ".json"
+    $file = "../../words/" + $fileName + ".json"
     //set mode of file to writable.
     chmod($file, 0777);
     $f = fopen($file, "w+") or die("fopen failed");
