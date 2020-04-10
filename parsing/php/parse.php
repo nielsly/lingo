951f2b6a0d5594efabcf5f0ef3689ee66227ff3f
Niels Gorter <nierls@live.nl>
2020-04-11 00:50:19 +0200

    Fix parsing and fetching json

--- parsing/php/parse.php
+++ parsing/php/parse.php
@@ -1,6 +1,6 @@
 <?php 
     $data = $_POST['objectString'];
     $name = $_POST['fileName'];
-    $file = '../../words/' + $fileName + '.json';
+    $file = '../../words/' . $name . '.json';
     file_put_contents($file, $data);
 ?>
