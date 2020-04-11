453fc1ffdf70fa0dd6ef490ccbea57bd73d22263
Niels Gorter <nierls@live.nl>
2020-04-11 04:09:40 +0200

    Fix header

--- php/suggestions.php
+++ php/suggestions.php
@@ -5,5 +5,5 @@ foreach ($_POST['suggestion'] as $key => $value) {
     $suggestions .= "\r\n" . value;
 }
 file_put_contents('../suggestions/' . $size, $suggestions, FILE_APPEND | LOCK_EX);
-header('../');
+header('Location: ../');
 ?>
